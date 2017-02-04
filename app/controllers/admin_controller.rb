b67d3f15468863d372b6e3db3f325641ed8cdb45
Eric Conde <condeeric@live.com>
2017-02-05 01:57:11 +0800

    allow devise to use own layout

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,13 @@
 class AdminController < ApplicationController
+
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "admin"
+    end
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -16,4 +18,15 @@ class ApplicationController < ActionController::Base
       u.permit(:email, :password, :password_confirmation, :current_password)
     end
   end
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
+  
 end
