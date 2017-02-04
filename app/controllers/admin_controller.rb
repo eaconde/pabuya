73ce25d25f8aa526213734bce9e381afb5d9ce88
Eric Conde <condeeric@live.com>
2017-02-05 02:14:54 +0800

    secure admin pages from normal user

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,13 @@
 class AdminController < ApplicationController
-
+  before_filter :ensure_admin
 
   private
+
+  def ensure_admin
+    unless current_user.admin?
+      render :file => "public/404.html", layout: false, :status => :unauthorized
+    end
+  end
 
   def layout_by_resource
     devise_controller? ? "devise" : "admin"
