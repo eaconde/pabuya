a700ee572c6cbb33c10db8bc7f2fe896522859f5
Eric Conde <condeeric@live.com>
2017-02-07 23:55:04 +0800

    add meta for approved/declined

--- app/models/user_event_submission.rb
+++ app/models/user_event_submission.rb
@@ -9,18 +9,31 @@ class UserEventSubmission < ActiveRecord::Base
     declined: "Declined"
   }
 
+  SUBMISSION_STATUS.each do |k,v|
+    define_method("#{k.to_s}?".to_sym) do
+      status == k.to_s
+    end
+  end
+
   validates :user, presence: true
   validates :event, presence: true
   validates :attachment, presence: true
   validates :status, presence: true, unless: :new_record?
   validates :points, presence: true, numericality: { greater_than: 0 },
-              if: lambda { |s| s.status == 'approved' }
+              if: lambda { |s| s.approved? }
+
+  before_update do |submission|
+    submission.points = 0 if submission.declined?
+  end
 
   after_update do |submission|
     submission.date_processed = Time.now
+    # send email to user
   end
 
   def processed?
     !self.date_processed.blank?
   end
+
+
 end
